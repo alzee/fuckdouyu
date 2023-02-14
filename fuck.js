761cb4676d5ac816a252909880209880a2f0d14a
Al Zee <z@alz.ee>
2023-02-14 20:57:16 +0800

    nth

---
+++ fuck.js
@@ -0,0 +1,10 @@
+let body = document.querySelector('body');
+
+/*
+setTimeout(() => {
+  body.classList = 'is-fullScreenPage';
+}, 5000);
+setTimeout(() => {
+  body.classList = 'is-fullScreenPage is-fullScreenAll is-fullScreenAll';
+}, 6000);
+*/
